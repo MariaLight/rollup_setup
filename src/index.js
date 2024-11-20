9df77ea9bb0ad5ba50154f908c70d1d4bcaae798
MariaLight <61028743+MariaLight@users.noreply.github.com>
2024-11-21 01:05:40 +1200

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./index.css";
+import logo from './assets/javascript.png';
+
+console.log('Hello World!');
+
+const title = document.createElement('h1');
+title.textContent = 'I love JavaScript';
+const image = document.createElement('img');
+image.src = logo;
+
+[title, image].forEach(element => {
+    
+    document.body.append(element);
+});
